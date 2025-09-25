3213068dc26ffc585298f188abb9a6f26d85cf94
randika26 <randikadwiputera95@gmail.com>
2025-09-25 11:19:37 +0700

    Perhitungan Total

--- Penjualan.php
+++ Penjualan.php
@@ -31,4 +31,12 @@ for ($i = 0; $i < $jumlah_pembelian; $i++) {
     ];
 }
 
+// Tampilkan detail pembelian
+echo "<h3>Detail Pembelian</h3>";
+foreach ($beli as $item) {
+    echo "Nama Barang : {$item['nama']} <br>";
+    echo "Harga Barang: Rp. " . number_format($item['harga'], 0, ',', '.') . " <br>";
+    echo "Jumlah Beli : {$item['jumlah']} <br>";
+    echo "Total Harga : Rp. " . number_format($item['total'], 0, ',', '.') . " <br><br>";
+}
 ?>
