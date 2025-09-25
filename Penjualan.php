0200c51f9b36949e23858e5888b81312eb4ee352
randika26 <randikadwiputera95@gmail.com>
2025-09-25 11:09:00 +0700

    Setup Awal

---
+++ Penjualan.php
@@ -0,0 +1,7 @@
+<?php
+// Daftar nama barang & harga dipisah
+$namaBarang = ["Sabun", "Shampoo", "Parfum", "Sunscreen", "Bedak"];
+$hargaBarang = [3000, 5000, 50000, 350000, 15000];
+
+echo "<h1>--POLGAN MART--</h1>";
+?>
